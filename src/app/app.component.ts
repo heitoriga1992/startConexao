03b076285fe431b007b1a4b14168137c81c72cf2
Heitor Iga <33259441+heitoriga@users.noreply.github.com>
2020-04-03 14:45:14 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'startConexao';
+export class AppComponent implements OnInit {
+  digitalData = {
+    site: {
+      nome: 'Banco Pan'
+    },
+    page: {
+      secao: 'CRM',
+      subsecao1: 'LandingPage',
+      estadoLogin: 'NL',
+      nome: 'BP:CRM:LandingCRM:LpPortasAbertas'
+    }
+  };
+  ngOnInit() {
+
+    console.log(this.digitalData);
+  }
+
 }
